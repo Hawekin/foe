5d64ff200b010d29b2dc20900f3bdd815d17ad14
DESKTOP-HQR35L3\Moderbord <xoshen@live.se>
2018-04-02 17:57:01 +0200

    Added a few races for necessary comparison when describing skin, fur, etc. Also added comparables for hide and feathers

--- js/body/bodypart.js
+++ js/body/bodypart.js
@@ -8,15 +8,32 @@ BodyPart.HasFur = function(race) {
 	return race.isRace(
 		Race.Canine,
 		Race.Feline,
+		Race.Rabbit,
 		Race.Goat,
 		Race.Sheep,
 		Race.Musteline,
-		Race.Rabbit)
+		Race.Rabbit,
+		Race.Wolf,
+		Race.Tiger,
+		Race.Panther,
+		Race.Jaguar,
+		Race.Puma,
+		Race.Lynx,
+		Race.Lion,
+		Race.Dog,
+		Race.Fox,
+		Race.Jackal,
+		Race.Hyena)
 		||
 		race.isRaceNotParent(Race.Deer);
 }
 BodyPart.HasScales = function(race) {
-	return race.isRace(Race.Reptile);
+	return race.isRace(
+		Race.Reptile,
+		Race.Dragon,
+		Race.Snake,
+		Race.Lizard,
+		Race.Salamander);
 }
 BodyPart.HasSkin = function(race) {
 	return race.isRace(
@@ -25,7 +42,21 @@ BodyPart.HasSkin = function(race) {
 		Race.Demon,
 		Race.Dryad);
 }
-
+BodyPart.HasHide = function(race) {
+	return race.isRace(
+		Race.Horse,
+		Race.Zebra,
+		Race.Cow,
+		Race.Deer,
+		Race.Goat,
+		Race.Satyr,
+		Race.Sheep);
+}
+BodyPart.HasFeathers = function(race) {
+	return race.isRace(
+		Race.Avian,
+		Race.Gryphon);
+}
 BodyPartType = {
 	cock   : 0,
 	vagina : 1,
